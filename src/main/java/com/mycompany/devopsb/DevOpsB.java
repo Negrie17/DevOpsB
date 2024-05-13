ae083c5e4d6e3520b32d65657f393874eefaa8da
Negrie17 <220757534@ump.ac.za>
2024-05-13 02:35:01 +0200

    My first commit

---
+++ src/main/java/com/mycompany/devopsb/DevOpsB.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.devopsb;
+
+/**
+ *
+ * @author Student
+ */
+public class DevOpsB {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Rea in the World!");
+    }
+}
